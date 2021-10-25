4fd0405dea5e104da1a374dbb0b0120b51c0e9d6
endalk <eb808826@gmail.com>
2021-10-25 12:23:28 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="container header">
+            <h1>Hello world</h1>
+        </div>
+    );
+}
+
+export default App;
